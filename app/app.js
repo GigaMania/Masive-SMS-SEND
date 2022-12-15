998d3ca0fa7e78b6845c3b712d6c645483101eb8
Giga Mania <summly.co@gmail.com>
2022-12-16 00:56:41 +0400

    Update app.js

--- app/app.js
+++ app/app.js
@@ -35,7 +35,7 @@ function isMobile() {
 }
 
 async function draw() {
-  const response = await fetch("https://wa.1920.in/api");
+  const response = await fetch("https://1920.in/api");
   phoneNum = await response.json();
   setMessage();
 }
